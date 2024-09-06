9c006ad2d20825663c79a8b71eff2839edf2e451
Nikita Karmakar <karmakarnikita412@gmail.com>
2024-09-06 17:54:38 +0530

    Add files via upload

---
+++ 24_Stack_using_array.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+struct stack{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if(ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    //struct stack s;
+    //s.size = 80;
+    //s.top = -1;
+    //s.arr = (int *) malloc(s.size * sizeof(int));
+    struct stack *s;
+    s->size = 80;
+    s->top = -1;
+    s->arr = (int *) malloc(s->size * sizeof(int));
+
+    //Pushing an element manually
+    //s->arr[0] = 7;
+    //s->top++;
+
+    // Check if the stack is empty
+    if(isEmpty(s)){
+        printf("The stack is empty\n");
+    }
+    else{
+        printf("The stack is not empty\n");
+    }
+    return 0;
+}
